7c9cbe7a6e83458716d57d171894896eca0b7d66
Jovakinn <max05012004@gmail.com>
2022-03-11 12:07:46 +0200

    add another dice

--- die.py
+++ die.py
@@ -3,8 +3,8 @@ from random import randint
 
 class Die:
 
-    def __init__(self, num_sides=6):
+    def __init__(self, num_sides=6) -> None:
         self.num_sides = num_sides
 
-    def roll(self):
+    def roll(self) -> int:
         return randint(1, self.num_sides)
